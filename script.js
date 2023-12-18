f28f6391e5bff107996d29aa6a71bb409adab825
Anton Knutsson <cykelkorg95@gmail.com>
2023-12-18 14:05:42 +0100

    pseudokod för första uppgiften

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // G-Version - Split the Nota
 // Skriv din pseudokod innanför nedanstående kommentarsblock
 /*
+// För att göra det enklare så har jag valt att bryta ned detta i till sex punkter
+// 1. Ange totala kostnaden - let totalCost = 0;
+// 2. Ange antal vänner - let numberOfFriends = 0;
+// 3. Ange procent dricks - let tipAmount = 0;
+// 4. Beräkna kostnaden ink dricks - totalCostWithTip 
+//    För att räkna ut - totalCostWithTip = totalCost + tipAmount;?
+// 5. Dela den totala kostnaden med antal vänner - 
+//    För att räkna ut - costPerFriend = totalCostWithTip / numberOfFriends;
+// 6. Visa hur mycket varje vän skall betala -  costPerFriend
 
+let totalCost = läs in användarinput("vad är den totala kostnaden?");
+let numberOfFriends = läs in användarinput("Hur många är ni som skall dela på kostnaden?");
+let tipAmount = läs in användarinput("Hur mucket vill ni lämna i dricks?");
+
+let totalCostWithTip = totalCost + (totalCost * tipAmount);
+
+let costPerFriend = totalCostWithTip / numberOfFriends;
+
+skriv ut ("Varje person skall betala: " + costPerFriend);
 
 */
 
